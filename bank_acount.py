3ea2680a58f58db5244381cd772b4611a7d4fe27
Prachi Shelgikar <praachi.shelgikar@gmail.com>
2021-04-03 04:41:00 +0000

    Initial Commit

---
+++ bank_acount.py
@@ -0,0 +1,29 @@
+# FIXME: Add Bank Account class definition here
+
+# You can see how this class should be used below. Make sure you
+# run your code and test it out to make sure it works.
+
+# account_1 = BankAccount()
+# account_2 = BankAccount()
+# account_3 = BankAccount(100)
+# account_3.balance
+#   >>> 100
+# account_1.deposit(100)
+# account_1.balance
+#   >>> 100
+# account_2.deposit(50)
+# account_2.balance
+#   >>> 50
+# account_3.deposit(75)
+# account_3.balance
+#   >>> 175
+# account_2.withdraw(10)
+# account_2.balance
+#   >>> 40
+# account_1.withdraw(10)
+# account_1.balance
+#   >>> 90
+# account_2.withdraw(50)
+#   >>> "Invalid transaction: not enough funds to withdraw $50." 
+# account_2.balance
+#   >>> 40    # still 40 since the previous transaction was not successful
